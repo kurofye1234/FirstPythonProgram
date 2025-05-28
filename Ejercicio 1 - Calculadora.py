f7b64453138ab5848c5378e30505f41382c990d3
kurofye1234 <20028245@es.uveg.edu.mx>
2025-05-28 09:38:04 -0600

    Second Program

---
+++ Ejercicio 1 - Calculadora.py
@@ -0,0 +1,28 @@
+##Ejercicio 1 - Calculadora 
+
+print("Bienvenido a la calculadora") 
+
+try:
+    numero1 = int(input("Ingresa el primer numero: ")) 
+    numero2= int(input("Ingresa el segundo numero:"))  
+
+except ValueError: 
+    print("Numero no valido")  
+
+print("Indica que operacion quieres hacer?") 
+opcion = input("+, -, *, /: ") 
+
+if opcion == "+":
+    resultado = numero1 + numero2 
+    print(f"La suma de {numero1} y {numero2} es: {resultado}") 
+elif opcion == "-":
+    resultado = numero1 - numero2 
+    print(f"La resta de {numero1} y {numero2} es: {resultado}") 
+elif opcion == "*":
+    resultado = numero1 * numero2 
+    print(f"La multiplicacion de {numero1} y {numero2} es: {resultado}") 
+elif opcion == "/":
+    resultado = numero1 / numero2 
+    print(f"La division de {numero1} y {numero2} es: {resultado}") 
+else:
+    print("Operacion no valida") 
--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,14 @@
-print ("Hello World")
+print ("Hello World") 
+nombre_variable = "Juan" 
+edad_variable = 25 
+print ("Hello " + nombre_variable + " Tienes " + str(edad_variable) + " años")
+
+a =10 
+b = 15
+sum = a + b 
+print ("La suma de " + str(a) + " y " + str(b) + " es: " + str(sum)) 
+
+input("Hola! Buenos dias, presiona enter para continuar")
+
+edad_variable = (int(input( nombre_variable + " Cuantos Años tienes?")))  
+print(f"tienes {edad_variable} años") 
